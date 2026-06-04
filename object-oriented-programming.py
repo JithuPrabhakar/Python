a220bc35f147533482f84387c78a66750b99ed3e
JithuPrabhakar <join.jitz@gmail.com>
2026-06-04 09:02:25 +0530

    completes OOP

--- object-oriented-programming.py
+++ object-oriented-programming.py
@@ -166,3 +166,148 @@ class Iphone_17(Iphone):
     pass
 class Iphone_17_pro_max(Iphone_17):
     pass
+
+
+# Polymorphism - allows same method, function or operator to behave differently based on context or object type - enhances code flexibility and reusabillity
+
+# Funciton/Method - a single function or method behaves differently based on the number or data type of the arguments
+# print('123.25', 123)
+
+# user defined function polymorphism
+class Student:
+    def show_details(self, name=None): # different number of arguments
+        if name:
+            print("Student name provided")
+        else:
+            print("No name provided")
+    
+s1 = Student()
+# s1.show_details()
+
+class Calculator:
+    def add(a, b): # different types of arguments
+        if isinstance(a, int) and isinstance(b, int):
+            return int(a) + int (b)
+        elif isinstance(a, str) and isinstance(b, str):
+            return a + b
+        else:
+            return f"a and b are not supported data types"
+        
+c = Calculator()
+# c.add(1, 2)
+# c.add('1', '2')
+
+# Operator overloading - lets define custom behaviour for python operators
+class Student:
+    def __init__(self, marks):
+        self.marks = marks
+        
+    def __add__(self, other):
+        return self.marks - other.marks
+    
+s1 = Student(80)
+s2 = Student(85)
+# print(s1 + s2)
+
+# Duck Typing - type or class of an object is less important than the methods it defines
+class Student:
+    def code(self):
+        print("Student is coding")
+        
+class Teacher:
+    def code(self):
+        print("Teacher is coding")
+        
+def start_coding(person):
+    person.code()
+    
+s = Student()
+t = Teacher()
+
+# start_coding(s)
+# start_coding(t)
+
+# Method overloading - multiple methods with same name but different parameters, this is not supported by python natively 
+# but can be mimicked using default arguments
+class Student:
+    def show_details(name=None, *args):
+        pass
+
+# Method Overriding - allows a method in Child class to redefine the method in Parent class
+class Person:
+    def show():
+        pass
+class Student(Person):
+    def show():
+        pass
+    
+# s = Student()
+
+
+# Encapsulation - wrapping of methods and attributes to a single unit (class) and restricting direct access
+class Student:
+    def __init__(self):
+        self.__name = ""
+    
+    def set_name(self, name): # setter
+        if name.isalpha():
+            self.__name = name
+        else:
+            print("Invalid format")
+            
+    def get_name(self): # getter
+        return self.__name
+    
+# Abstraction - hiding implementation details and showing only the essential features
+from abc import ABC, abstractmethod
+
+class Person(ABC):
+    @abstractmethod
+    def work(self):
+        pass
+    
+class Student(Person):
+    def work(self):
+        print("student is studying")
+        
+
+# Constructor - __init__() - magic (special) method in python which is automatically called when creating a new object.
+# It is used to initialize object attributes
+class Sample:
+    def __init__(self, name): # Constructor
+        self.name = name # initialization
+
+s1 = Sample("demo")
+
+# Destructor - __del__() - automatically called when an object is about to be destructed
+class Sample:
+    def __init__(self, name): # Constructor
+        self.name = name # initialization
+        
+    def __del__(self):
+        print(f"{self.name} is deleted")
+
+s1 = Sample("demo")
+del s1
+
+# Object lifecycle - stages of an object
+# 1. Objet creation - __init__()
+# 2. Object usage - accessing attribues, calling methods
+# 3. Object destruction - __del__()
+
+# Class method and static method
+class Laptop:
+    screen_size = 15.6 # Class attribute
+    def __init__(self, name):
+        self.name = name # instance attribute
+        
+    def hello(self): # instance method
+        pass
+    
+    @classmethod
+    def hi(cls):
+        print(f"Hi, laptop kittiyo? cls.screen_size()")
+    
+    @staticmethod
+    def hi():
+        pass
