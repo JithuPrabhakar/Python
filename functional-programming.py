be359db7dc8c911ffe95e883d308667fc734db3a
JithuPrabhakar <join.jitz@gmail.com>
2026-06-15 17:43:18 +0530

    iterators and generators

--- functional-programming.py
+++ functional-programming.py
@@ -34,7 +34,7 @@ evens = list(filter(lambda x : x % 2 == 0, nums))
 # evens = nums.filter((x) => x % 2 == 0) in js
 
 # reduce() - combine items in an itrerable into a single value
-from functools import reduce
+from functools import reduce # functools contains necessary tools for functional programming
 sum_of_items = reduce(lambda x, y: x + y, nums)
 # sumOfItems = nums.reduce((x, y) => x + y, 0) in js
 
@@ -45,3 +45,85 @@ def sum_of_items(x, y):
 names = ["Suresh", "Ramesh", "Ratheesh", "Anish"]
 names.sort(key=lambda x : len(x))
 # print(names)
+
+# Lambda constructor in Python - refers to a lambda function inside a constructor
+class Student:
+    def __init__(self, name, marks):
+        self.name = name
+        self.marks = marks
+        self.status = (lambda m : "Pass" if m >= 40 else "Fail")(marks)
+        
+s1 = Student("Akhil", 30)
+# print(s1.status)
+
+# Comprehensions
+# list comprehension - squares = [x*x for x in range(5)]
+# set comprehension - unique = {x for x in [1, 2, 2, 3]}
+# dict comprehension - square_dict = {x : x * x for x in range(5)}
+
+string = "String"
+list_of_char = [char for char in string]
+# print(list_of_char)
+
+numbers = [1, 3, 5, 7, 9]
+num_dict = {x : x**3 for x in numbers}
+
+sentence = "the quick brown fox jumps over the lazy dog"
+
+even_odd = [x if x%2==0 else "Odd" for x in range(1, 11)]
+
+numbers = [[1, 3, 5], [7, 9, 2], [3, 5, 7], [9, 1, 2]]
+
+# Iterables and Iterators : Iterable is a collection and Iterator is the object that refers to the current item in the collection
+
+# fruits = ['apple', 'orange', 'grape', 'pineapple', 'mango']
+
+# for fruit in fruits:
+#     print(fruit)
+
+# it = iter(fruits)
+
+# while True:
+#     try:
+#         item = next(it)
+#         print(item)
+#     except StopIteration:
+#         
+
+# print(next(it))
+# print(next(it))
+# print("next item")
+# print(next(it))
+
+# Generator
+def counter():
+    n = 1
+    while True:
+        yield n
+        n += 1
+    
+count = counter()
+# print(next(count))
+# print(next(count))
+
+# __iter__() and __next__()
+
+class Count: 
+    def __init__(self, min, max): 
+        self.max = max
+        self.min = min
+        # self.step = step
+        if min == 0:
+            self.current = -1
+        if min > 0:
+            self.current = min-1
+    def __iter__(self): 
+        return self
+    def __next__(self):
+        if self.current < self.max-1:
+            self.current += 1
+            return self.current
+        raise StopIteration
+    
+# for num in Count(2,6):
+#     print(num)
